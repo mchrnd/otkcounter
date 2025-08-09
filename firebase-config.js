9d376f276011a94767c7123fdc2a4a5f497ec581
mchrnd <150801780+mchrnd@users.noreply.github.com>
2025-08-09 14:33:35 +0900

    config Delete

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC4xFRjDTO8blmWriavMIxbFuwlzZvRB3g",
-    authDomain: "mycountertest.firebaseapp.com",
-    databaseURL: "https://mycountertest-default-rtdb.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "mycountertest",
-    storageBucket: "mycountertest.firebasestorage.app",
-    messagingSenderId: "451353299994",
-    appId: "1:451353299994:web:4c4b6ca04e09483bf5d5a3",
-    measurementId: "G-ZYY84567ZK"
+    storageBucket: "m",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
